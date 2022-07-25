8ed38eb8bc6c72673c0533c1c3859db70acd9a12
Akshat Dhir <akshadhir@gmail.com>
2022-07-25 14:25:54 +0200

    Loader

---
+++ data_transform.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+
+def main(data_input):
+    ## transform logic
+
+
+
+if "__name__" == "__main__" :
+    data_load = pd.read_excel(path)
+    data_load_df = pd.DataFrame(data_load)
+    main(data_load_df)
