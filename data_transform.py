72954a69500ec61134565ca4167566c9bc2d2860
Akshat Dhir <akshadhir@gmail.com>
2022-07-26 12:18:28 +0200

    added data

--- data_transform.py
+++ data_transform.py
@@ -1,13 +1,16 @@
 import numpy as np
 import pandas as pd
+import sys
 
 
-def main(data_input):
+def main(data_input_df):
     ## transform logic
+    print(data_input_df.head())
 
 
-
-if "__name__" == "__main__" :
+if __name__ == '__main__':
+    path = sys.argv[1]
+    print(path)
     data_load = pd.read_excel(path)
     data_load_df = pd.DataFrame(data_load)
     main(data_load_df)
